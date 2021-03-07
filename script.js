273376edd8d3863ea0de4226813567f0ec66e805
Paulo de Sa <paulodesa27@gmail.com>
2021-03-06 23:40:28 -0500

    homework commit

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const number = "1234567890"
+const specialChar = "!#$%&*"
+const capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lower = "abcdefghijklmnopqrstuvwxyz"
+const passwordLength = ''
+
+function generatePassword() {
+  var finalPassword = "";
+  var characters "";
+
+  var passwordLength = window.prompt(
+    "Please choose between a password between 8 and 128 characters");
+    if (passwordLength < 8 || passwordLength > 128)
+    window.prompt ("Please choose between 8 and 128 characters")
+
+    for (let i = 0; i < passwordLength; i++) {
+      console.log();
+    }
+}
+
